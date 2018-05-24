41fb7265df711fcffcf9bad066b8fc9e823518d6
Bruno Santos <bruno@mini-de-bruno.galpao.local>
2018-05-24 12:06:35 -0300

    json completo com os dados

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.get('/html', function(req, res){
 app.get('/scrape', function(req, res){
 
 //url = 'http://localhost:8081/html/';
-url = 'https://pt.wikipedia.org/wiki/Carbono';
+url = 'https://pt.wikipedia.org/wiki/Lítio';
 request(url, function(error, response, html){
     if(!error){
         
 
         var title, release, rating;
-        var json = { assunto : "", dado : ""};
+        var json = { };
         var array = [];
         var i;
         var $ = cheerio.load(html);
@@ -26,7 +26,7 @@ request(url, function(error, response, html){
         $('.infobox_v2 tbody tr td').each((i, element) => {
             const cheerioElement = $(element);
             const avatar = cheerioElement.text();
-          
+            console.log(avatar);
             //avatar = avatar.replace(/^\s+|\s+$/g,"");
    
            
@@ -36,20 +36,43 @@ request(url, function(error, response, html){
                   (avatar == "Grupo, período, bloco")||
                   (avatar == "Densidade, dureza")||
                   (avatar == "Número CAS")||
-
+                  (avatar == "Massa atômica")||
+                  (avatar == "Raio atómico (calculado)")||
+                  (avatar == "Raio de Van der Waals")||
+                  (avatar == "Configuração electrónica")||
+                  (avatar == "Elétrons (por nível de energia)")||
+                  (avatar == "Estado(s) de oxidação")||
+                  (avatar == "Estrutura cristalina")||
+                  (avatar == "Estado da matéria")||
+                  (avatar == "Ponto de fusão")||
+                  (avatar == "Ponto de ebulição")||
+                  (avatar == "Entalpia de fusão")||
+                  (avatar == "Entalpia de vaporização")||
+                  (avatar == "Volume molar")||
+                  (avatar == "Pressão de vapor")||
+                  (avatar == "Velocidade do som")||
+                  (avatar == "Classe magnética")||
+                  (avatar == "Eletronegatividade (Pauling)")||
+                  (avatar == "Calor específico")||
+                  (avatar == "Condutividade elétrica")||
+                  (avatar == "Condutividade térmica")||
+                  (avatar == "1º Potencial de ionização")||
+                  (avatar == "2º Potencial de ionização")||
                   (avatar == "3º Potencial de ionização")
                 ){
                
                 liberar_concact = 1;
                 titulo = avatar;
+
             }else{
                 if(liberar_concact == 1){
-                    i = i +1;
-                    json[i] = {titulo :titulo, dado:avatar};
-                    
-                    liberar_concact = 0; 
-                    titulo = '';
-                    
+                    if(avatar){
+                        i = i +1;
+                        json[i] = {titulo :titulo, dado:avatar};
+                        
+                        liberar_concact = 0; 
+                        titulo = '';
+                    } 
                 }
             }
 
