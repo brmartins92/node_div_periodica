9f2973e93ea7ed1a85e02b7b23b52e3be2c7af0a
Bruno Santos <bruno@mini-de-bruno.galpao.local>
2018-05-29 16:59:45 -0300

    acertos

--- server.js
+++ server.js
@@ -20,7 +20,93 @@ app.get('/html', function(req, res){
     res.sendFile('html/tabela.html', {root: __dirname });
 })
 
-app.get('/scrape/:elemento', function(req, res){
+app.get('/props/:elemento', function(req, res){
+var elemento = req.params.elemento;
+//url = 'http://localhost:8081/html/';
+url = 'https://pt.wikipedia.org/wiki/'+elemento;
+request(url, function(error, response, html){
+    if(!error){
+        var title, release, rating;
+        var json = { };
+        var array = [];
+        var i;
+        var $ = cheerio.load(html);
+        var liberar_concact;
+        var titulo;
+        var src = '';
+        src = 'http://www.buritama.sp.leg.br/imagens/parlamentares-2013-2016/sem-foto.jpg/image'; 
+        json['img'] = {src :src};
+        $('.infobox_v2 tbody tr td').each((i, element) => {
+            const cheerioElement = $(element);
+            const avatar = cheerioElement.text();
+            
+            src = cheerioElement.find('.image').children('img').eq(0).attr('src');
+            //var src = $('img').attr("src");
+            if((src)&&(  (src.includes(".jpg"))|| (src.includes(".JPG"))  )){
+                console.log(src);
+                src = 'https:'+src;
+                json['img'] = {src :src};
+            }
+              
+            if(   
+                  (avatar == "Nome, símbolo, número")||             
+                  (avatar == "Série química")|| 
+                  (avatar == "Grupo, período, bloco")||
+                  (avatar == "Densidade, dureza")||
+                  (avatar == "Número CAS")||
+                  (avatar == "Massa atômica")||
+                  (avatar == "Raio atómico (calculado)")||
+                  (avatar == "Raio de Van der Waals")||
+                  (avatar == "Configuração electrónica")||
+                  (avatar == "Elétrons (por nível de energia)")||
+                  (avatar == "Estado(s) de oxidação")||
+                  (avatar == "Estrutura cristalina")||
+                  (avatar == "Estado da matéria")||
+                  (avatar == "Ponto de fusão")||
+                  (avatar == "Ponto de ebulição")||
+                  (avatar == "Entalpia de fusão")||
+                  (avatar == "Entalpia de vaporização")||
+                  (avatar == "Volume molar")||
+                  (avatar == "Pressão de vapor")||
+                  (avatar == "Velocidade do som")||
+                  (avatar == "Classe magnética")||
+                  (avatar == "Eletronegatividade (Pauling)")||
+                  (avatar == "Calor específico")||
+                  (avatar == "Condutividade elétrica")||
+                  (avatar == "Condutividade térmica")||
+                  (avatar == "1º Potencial de ionização")||
+                  (avatar == "2º Potencial de ionização")||
+                  (avatar == "3º Potencial de ionização")
+                ){
+               
+                liberar_concact = 1;
+                titulo = avatar;
+
+            }else{
+                if(liberar_concact == 1){
+                    if(avatar){
+                        i = i +1;
+                        json[i] = {titulo :titulo, dado:avatar};
+                        
+                        liberar_concact = 0; 
+                        titulo = '';
+                    } 
+                }
+            }
+
+        });
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(json));
+
+}
+
+
+    }) ; // do inicio
+}) //fim /scrape
+
+
+app.get('/summary/:elemento', function(req, res){
 var elemento = req.params.elemento;
 //url = 'http://localhost:8081/html/';
 url = 'https://pt.wikipedia.org/wiki/'+elemento;
@@ -104,7 +190,8 @@ request(url, function(error, response, html){
 //res.send('Check your console!')
 
     }) ; // do inicio
-}) //do inicio 
+}) //fim resumo
+
 
 
 //app.listen('8081')
